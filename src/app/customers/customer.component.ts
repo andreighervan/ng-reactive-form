837c83203159dd35ddfb4bfbb8ad3be8057a4f3b
andreighervan <andrei.ghervan8@gmail.com>
2020-08-24 09:46:42 +0300

    add customer forms

---
+++ src/app/customers/customer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss']
+})
+export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
+  customer = new Customer();
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      sendCatalog: true
+    })
+  }
+
+  save(): void {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm));
+  }
+}
